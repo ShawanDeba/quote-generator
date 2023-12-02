f8a517033f7825670940dfe391f8633a2f31e0cf
shawandeba@gmail.com <shawandeba@gmail.com>
2023-12-02 18:13:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const api_url = "https://api.quotable.io/random";
+let quote  = document.getElementById('quote');
+let author = document.getElementById('author');
+
+async function getquote(url){
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    quote.innerHTML = data.content;
+    author.innerHTML = data.author;
+    
+}
+
+getquote(api_url)
+
+
+function tweet(){
+   
+    window.open('https://twitter.com/intent/tweet?text='+quote.innerHTML + "  --by "+author.innerHTML,"Tweet Window","width=600,height=400")
+}
